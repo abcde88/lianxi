0dafbd06b404805767fc02af379f5773a98f3247
abcde88 <1362118860@qq.com>
2019-10-16 15:28:44 +0800

    www分支

--- gitshangchuan/app/src/main/java/com/example/gitshangchuan/MainActivity.java
+++ gitshangchuan/app/src/main/java/com/example/gitshangchuan/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.gitshangchuan;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /**
+     * Hello World!
+     */
+    private TextView mQwe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mQwe = (TextView) findViewById(R.id.qwe);
+        mQwe.setOnClickListener(this);
+        Toast.makeText(this, "123", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.qwe:
+                break;
+        }
     }
 }
